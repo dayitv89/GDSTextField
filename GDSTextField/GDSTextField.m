a233cecd2d62f46fd5a6a4e579752a3592ae1d25
GauravDS <gaurav.sharma@punchh.com>
2015-05-01 16:44:14 +0530

    Minor updates

--- GDSTextField/GDSTextField.m
+++ GDSTextField/GDSTextField.m
@@ -28,6 +28,26 @@
     }
     [super deleteBackward];
 }
+
+- (BOOL)keyboardInputShouldDelete:(UITextField *)textField {
+    BOOL shouldDelete = YES;
+    if ([UITextField instancesRespondToSelector:_cmd]) {
+        BOOL (*keyboardInputShouldDelete)(id, SEL, UITextField *) = (BOOL (*)(id, SEL, UITextField *))[UITextField instanceMethodForSelector:_cmd];
+        if (keyboardInputShouldDelete) {
+            shouldDelete = keyboardInputShouldDelete(self, _cmd, textField);
+        }
+    }
+
+    BOOL isIos8 = ([[[UIDevice currentDevice] systemVersion] intValue] == 8);
+    BOOL isLessThanIos8_3 = ([[[UIDevice currentDevice] systemVersion] floatValue] < 8.3f);
+
+    if (![textField.text length] && isIos8 && isLessThanIos8_3) {
+        [self deleteBackward];
+    }
+
+    return shouldDelete;
+}
+
 @end
 
 @interface GDSTextField()<MyGDSCustomFieldDelegate>{
